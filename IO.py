ab0606b960f16655003ab84c7909283f39786d4a
gitoutkai <kaihehau@hawaii.edu>
2025-03-19 13:21:56 -0700

    Create IO.py
    Hey Rob can you look at this and tell me why it wont work? just so i can fix it up. Can you grade it for credit after I make the changes?
    
    Thank-Kai

---
+++ IO.py
@@ -0,0 +1,65 @@
+#Condtionals0.1
+
+var1= 3
+var2= 54
+#here are my two variables  this sucks i couldn't do the one before this and i been working for 4 hours
+
+if (var1 > var2):
+	#this is the if statement, I experimented with these to create a bot that could kind of talk, 
+	print("var1 is bigger!")
+	#This is more or less how it worked, but it would print different things depending on who won F1
+
+print("Done!")
+
+
+if (var1 == var2):
+	print("var1 is equal to var2")
+#on my first try the print out said, you spent 30 million to tie with stroll
+
+if (var1 >= var2):
+	print ("var1 is greater then or equal to var 2")
+	#og print out said "Ferarri cant  win by more than (Print(var3)) points?"
+
+if (var1 > var2 and var 1 == var2):
+	print ("This is always true!")
+#because the number needs to be greater than the other or equal to each other because logic
+else:
+	Print("This is always true")
+#I used this when I wanted to create a bot that would talk with semi normal dialogue
+#prompted to answer a yes or no answer and the "no" was the else, ie: input(do u breakthelaw?) 
+#if then statement
+#if yes then more text
+#if no then end of conversation
+
+var3= input("enteryour own number", int)
+
+try:
+	var4 = int(var3)
+	if (var1 >var4):
+		print("Var1 is bigger")
+	else:
+		if(var1=var4):
+			print("var1 is the same as your number")
+		else
+			print("Var1 is smaller")
+
+except:
+	print("that is not a number")
+
+#i agree the except thing is super cool and I should have asked about it because I wasted a lot of time #without it
+
+print(var4)
+
+
+
+try:
+	var4 = int(var3)
+	if (var1>var4):
+		print("var1 is bigger")
+	elif (var1 < var4):
+		print("Var1 is smaller")
+
+
+except:
+	print("that is not a number")
+
